fa6b14abf70ccb7a4185a880d848b6c7fa3710aa
tmpanmitw <112293743+tmpanmitw@users.noreply.github.com>
2023-01-09 15:57:16 +0900

    feat : 초기 파일 생성
    feat : 초기 파일 생성

---
+++ butterfly-eff/src/test/java/com/example/butterflyeff/ButterflyEffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.butterflyeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterflyEffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
